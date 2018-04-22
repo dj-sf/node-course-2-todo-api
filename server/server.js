e97ccea2390c2e502d4d04de84edf55d0a5575ca
Jim Stricker <jmstricker93@gmail.com>
2018-04-22 10:39:19 -0400

    setup separate test and development envs

--- server/server.js
+++ server/server.js
@@ -1,3 +1,15 @@
+let env = process.env.NODE_ENV || 'development';
+console.log('env ******', env)
+
+if (env === 'development'){
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if (env === 'test'){
+    process.env.PORT = 3000;
+    process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
+
+
 const _ = require('lodash');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,7 +21,7 @@ let {User} = require('./models/user');
 
 
 let app = express();
-let port = process.env.PORT || 3000;
+let port = process.env.PORT;
 
 app.use(bodyParser.json());
 
