383ddf33a56e2e015f89a289f2f22c7d986aa6da
Jim Stricker <jmstricker93@gmail.com>
2018-04-23 19:35:32 -0400

    hash user password before saving

--- server/server.js
+++ server/server.js
@@ -130,7 +130,7 @@ app.get(`/users`, (req, res) =>  {
 
 app.post(`/users`, (req, res) => {
     let body = _.pick(req.body, ['email', 'password'])
-    let user = new User(body)
+    let user = new User(body);    
 
     user.save().then(() => {
         return user.generateAuthToken();
